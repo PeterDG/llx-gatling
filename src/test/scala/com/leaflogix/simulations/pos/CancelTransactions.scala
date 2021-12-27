3cc0d801a667ac0e8f09710849d51f90df3d2113
jjdiazatuesta <juan.diaz@dutchie.com>
2021-12-27 10:57:08 -0500

    adding .bat file to cancel all transactions

---
+++ src/test/scala/com/leaflogix/simulations/pos/CancelTransactions.scala
@@ -0,0 +1,14 @@
+package com.leaflogix.simulations.pos
+
+import com.leaflogix.httpProtocol
+import com.leaflogix.scenarios.pos.CancelTransactionBehavior
+import io.gatling.core.Predef._
+import ru.tinkoff.gatling.config.SimulationConfig._
+import ru.tinkoff.gatling.influxdb.Annotations
+
+class CancelTransactions extends Simulation with Annotations {
+  setUp(
+    CancelTransactionBehavior().inject(atOnceUsers(50)
+    )
+  ).protocols(httpProtocol)
+}
