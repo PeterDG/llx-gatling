7c18cca04f7c0d7cc0e5b1d172db2c8faf291bb2
jjdiazatuesta <juan.diaz@dutchie.com>
2022-03-30 14:39:31 -0500

    changing pos and boh api base url

--- src/test/scala/com/leaflogix/params/paths/BackendPaths.scala
+++ src/test/scala/com/leaflogix/params/paths/BackendPaths.scala
@@ -2,7 +2,7 @@ package com.leaflogix.params.paths
 
 object BackendPaths {
 
-  val BACKEND_URL = "https://parallel-uat.leaflogix.net"
+  val BACKEND_URL = "https://parallel-uat-api.leaflogix.net"
   val EMPLOYEE_LOGIN_PATH = "/api/posv3/user/EmployeeLogin"
   val RECEIVE = "/api/v2/inventory/receive"
   val GET_INVENTORY = "/api/v2/inventory/get-inventory"
